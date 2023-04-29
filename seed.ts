ad75444ec0a0a9d4edc7a1552944a42de003f4a4
vmtamil <122802163+vmtamil@users.noreply.github.com>
2023-04-29 13:23:48 +0530

    Add files via upload

--- seed.ts
+++ seed.ts
@@ -2,107 +2,57 @@ import { Prisma, PrismaClient } from "@Prisma/client";
 
 const client = new PrismaClient();
 
-const getbooks = (): Prisma.booksCreateInput[] => [
+const getevents = (): Prisma.eventsCreateInput[] => [
   {
-    book_id: "0001",
-    book_name: "Harry potter",
-    book_type: "New",
-    price: "456",
-    price_id: "1234",
-    availabe_for: "Sell",
-    category: "Fiction",
-    publisher: "Scholastic",
-    author_name: "Joanne Rowling",
-    description:
-      "The novels of whom are students at Hogwarts School of Witchcraft and Wizardry. ",
-    image: "jpeg",
-    ratings: "5",
-    language: "English",
-    discount: "10%",
-    readlist: "Favourite",
+    event_id : "0001",
+    name  : "xyz",
+    duration : "2 hours",
+    location : "Mysore",
+    organiser_details :"xyz",
+    organiser_details_id : "001",
   },
 ];
 
 [
     {
-      book_id: "0002",
-      book_name: "The Exorcist",
-      book_type: "New",
-      price: "788",
-      price_id: "1235",
-      availabe_for: "Rent",
-      category: "Horror",
-      publisher: "Harper & Row",
-      author_name: "William Peter Blatty",
-      description:
-        " The book details the demonic possession of eleven-year-old Regan MacNeil, the daughter of a famous actress, and the two priests who attempt to exorcise the demon.",
-      image: "jpeg",
-      ratings: "4.2",
-      language: "English",
-      discount: "32%",
-      readlist: "Reading",
+      event_id : "0002",
+      name  : "Abc",
+      duration : "3 hours",
+      location : "Bangalore",
+      organiser_details :"Abc",
+      organiser_details_id : "002",
     },
   ];
 
   [
     {
-      book_id: "0003",
-      book_name: "Wings of fire",
-      book_type: "New",
-      price: "982",
-      price_id: "1236",
-      availabe_for: "Lease",
-      category: "Non-Fiction",
-      publisher: "A.P.J Abdul Kalam",
-      author_name: "A.P.J Abdul Kalam",
-      description:
-        " It tells the narrative of a young boy from a poor family who rose through the ranks of Indian space research and missile programmes to become the country's president.",
-      image: "jpeg",
-      ratings: "4.8",
-      language: "English",
-      discount: "22%",
-      readlist: "On hold",
+      event_id : "0003",
+      name  : "Efg",
+      duration : "6 hours",
+      location : "Telengana",
+      organiser_details :"Efg",
+      organiser_details_id : "003",
     },
   ];
 
   [
     {
-      book_id: "0004",
-      book_name: "All the Light We Cannot See",
-      book_type: "Old",
-      price: "546",
-      price_id: "1237",
-      availabe_for: "Sale",
-      category: "Fiction",
-      publisher: "Simon & Schuster",
-      author_name: "Anthony Doerr",
-      description:
-        "  The story of the blind Marie-Laure, the self-doubting Werner Pfenning, and the scared old Etienne LeBlanc.",
-      image: "jpeg",
-      ratings: "4.1",
-      language: "English",
-      discount: "17%",
-      readlist: "Plan to read",
+      event_id : "0004",
+      name  : "SSS",
+      duration : "3 hours",
+      location : "Darvard",
+      organiser_details :"SSS",
+      organiser_details_id : "004",
     },
   ];
 
   [
     {
-      book_id: "0005",
-      book_name: "The Wager: A Tale of Shipwreck, Mutiny and Murder",
-      book_type: "New",
-      price: "999",
-      price_id: "1238",
-      availabe_for: "Rent",
-      category: "Non-Fiction",
-      publisher: "PenguinRandomHouse",
-      author_name: "David Grann",
-      description:
-        "  More than half survived the wreckage only to find themselves stranded on a desolate island.",
-      image: "jpeg",
-      ratings: "4.3",
-      language: "English",
-      discount: "45%",
-      readlist: "Favourite",
+      event_id : "0005",
+      name  : "SRR",
+      duration : "7 hourss",
+      location : "Hydrabad",
+      organiser_details :"SRR",
+      organiser_details_id : "005",
     },
   ];
